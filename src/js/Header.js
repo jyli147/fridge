45cd6aa846b8c1b6019a0726df88a501252ed0f3
Юля <jyli147@mail.ru>
2025-02-02 22:27:10 +0300

    Правки

--- src/js/Header.js
+++ src/js/Header.js
@@ -1,3 +1,27 @@
+class BaseError extends Error {
+  constructor(message, cause) {
+    super(message, cause);
+  }
+}
+
+class ValidationError extends BaseError {
+  constructor(fields, message) {
+    super(message ?? "Произошла непредвиденная ошибка", {
+      fields,
+    });
+
+    this.fields = !Array.isArray(fields) || fields.length === 0 ? null : fields;
+  }
+}
+
+class UnexpectedError extends BaseError {
+  static defaultMessage = "Произошла непредвиденная ошибка";
+
+  constructor(message, cause) {
+    super(message ?? UnexpectedError.defaultMessage, cause);
+  }
+}
+
 class Header {
   selectors = {
     root: "[data-js-header]",
@@ -84,26 +108,47 @@ class Header {
 new Header();
 
 // Модальное окно
+let isOpenModal = false;
+
+function openModal() {
+  if (!isOpenModal) {
+    isOpenModal = true;
+    document.getElementById("modal").classList.add("open");
+  }
+}
+
+function closeModal() {
+  if (isOpenModal) {
+    isOpenModal = false;
+    e.currentTarget.classList.remove("open");
+  }
+}
+
+// обработчики модального окна
 document.querySelectorAll("[data-js-button]").forEach((button) => {
   button.addEventListener("click", function () {
-    document.getElementById("modal").classList.add("open");
+    openModal();
   });
 });
 
 window.addEventListener(`keydown`, (e) => {
   if (e.key === "Escape") {
-    document.getElementById("modal").classList.remove("open");
+    closeModal();
   }
 });
 
 document.querySelector("#modal .modal-box").addEventListener("click", (e) => {
   e._isClickWithInModal = true;
-});
-document.getElementById("modal").addEventListener("click", (e) => {
-  if (e._isClickWithInModal) return;
-  e.currentTarget.classList.remove("open");
+  isOpenModal = false;
 });
 
+document.getElementById("modal").addEventListener("click", (e) => {
+  if (e._isClickWithInModal) return;
+
+  closeModal();
+});
+
+// пишу коментарий что это такое и зачем
 const inputs = document.querySelectorAll("[data-js-input]");
 const nameLabels = document.querySelectorAll("[data-js-name]");
 
@@ -122,80 +167,224 @@ inputs.forEach((input) => {
 });
 toggleVisibility();
 
-// Работа с формой
+// Совершение запрос
+async function sendRequest(phoneNumber, name) {
+  //   {
+  //     "statusCode": 400,
+  //     "error": "Bad Request",
+  //     "message": "Validation Error",
+  //     "extensions": {
+  //         "code": "VALIDATION_ERROR",
+  //         "message": "Validation Error",
+  //         "fields": [
+  //             {
+  //                 "name": "phoneNumber",
+  //                 "message": "Неверный номер телефона"
+  //             }
+  //         ]
+  //     }
+  // }
 
-const resultElement = document.querySelector(".result");
-const resultTextElement = document.querySelector(".result-text");
-const resultCloseElement = document.querySelector(".result-close");
-const createPostForms = document.querySelectorAll(".data__form");
-const loader = document.getElementById("loader");
-const telEerrorsElements = document.querySelectorAll(".tel-errors");
+  try {
+    const response = await fetch("http://localhost:5000/order", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({
+        phoneNumber,
+        name,
+        __test_delay: 2_000,
+      }),
+    });
 
-function formListener(form) {
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const formDataObject = findOrderData(form);
-    createOrder(formDataObject);
-  });
-}
+    const payload = await response.json();
 
-function findOrderData(form) {
-  const formData = new FormData(form);
-  return Object.fromEntries(formData);
-}
+    if (!response.ok) {
+      if (payload.extensions?.code === "VALIDATION_ERROR") {
+        if (
+          !Array.isArray(payload.extensions.fields) ||
+          payload.extensions.fields.length === 0
+        ) {
+          throw new ValidationError(null, payload.extensions.message);
+        }
 
-function createOrder(formDataObject) {
-  fetch("http://localhost:5000/order", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(formDataObject),
-  })
-    .then((response) => {
-      toggleLoader();
-
-      if (!response.ok) {
-        const errorMessage =
-          response.status === 404
-            ? "Не правильно заполнены поля"
-            : "Что-то пошло не так";
-        throw new Error(errorMessage);
+        throw new ValidationError(payload.extensions.fields);
       }
-      // Как обработать результат, чтобы попасть в ошибку?
+    }
 
-      return response.json();
+    return payload;
+  } catch (error) {
+    if (error instanceof BaseError) {
+      throw error;
+    }
+
+    throw UnexpectedError(undefined, error);
+  }
+}
+
+// IDLE -- ничего не происходит
+
+// PROCESSING -- выполнение запроса
+// SUCCEEDED -- запрос выполнен успешно
+// FAILED -- запрос выполнен с ошибкой
+
+function submitListenner(event) {
+  event.preventDefault();
+  const form = event.target;
+
+  const [phoneNumber, name] = [...form.querySelectorAll("[data-js-input]")].map(
+    (e) => e.value
+  );
+
+  // PROCESSING
+  const { submitButtonNode, submitButtonSavedText } =
+    processingStageLockUi(form);
+
+  // debugger;
+
+  sendRequest(phoneNumber, name)
+    .finally(() => {
+      // PROCESSING
+      processingStageUnlockUi(submitButtonNode, submitButtonSavedText);
     })
-    .then(createOrderOkHandler)
-    .catch(createOrderErrorHandler)
-    .finally(toggleLoader);
+    .then((result) => succeededStage(result, form))
+    .catch((error) => failedStageUpdateUi(error, form));
+
+  // debugger;
 }
 
-function toggleLoader() {
-  loader.classList.toggle("visually-hidden");
+//
+// PROCESSING
+//
+
+/**
+ * функция блокирует UI до запроса, чтобы не совершались повторные вызовы запроса из UI
+ * Эта функция возвращает ноду кнопки submit и текст с этой кнопки. Далее эти данные должны быть
+ * переданы в функцию unlockUi
+ */
+function processingStageLockUi(form) {
+  // ЗАДАЧА: в идеале  сделать на кнопке лоадер (спиннер)
+  const submitButtonNode = form.querySelector("[type=submit]");
+  const submitButtonSavedText = submitButtonNode.innerText;
+  submitButtonNode.innerHTML = '<span class="loader"></span>';
+  // debugger;
+  submitButtonNode.disabled = true;
+
+  return { submitButtonNode, submitButtonSavedText };
 }
 
-// Пока не поняла нужна ли эта функция
-// function inputsClear() {
-//   inputs.forEach((input) => {
-//     input.value = "";
-//   });
-// }
+/**
+ * функция разблокирует UI после запроса. Принимает данные из функции lockUi
+ */
+function processingStageUnlockUi(submitButtonNode, submitButtonSavedText) {
+  // ЗАДАЧА: убрать спинер и показать кнопку
+  submitButtonNode.innerText = submitButtonSavedText;
+  submitButtonNode.disabled = false;
+}
 
-function createOrderOkHandler(json) {
+//
+// SUCCEEDED
+//
+function succeededStage(result, form) {
+  // ЗАДАЧА: отобразить успешное завершение кейса отпраки запроса
+
+  const resultElement = document.querySelector(".result");
+  const resultTextElement = document.querySelector(".result-text");
   resultElement.classList.toggle("visually-hidden");
-  resultTextElement.innerHTML = json.message;
+  resultTextElement.innerText = result.message;
+  // Закрыть модалку
+  debugger;
+  console.log(result);
+  closeModal();
 }
 
-function createOrderErrorHandler(error) {
-  telEerrorsElements.forEach((element) => element.classList.toggle("show"));
-  // Когда убирать этот элемент?
+//
+// FAILED
+//
+function failedStageUpdateUi(error, form) {
+  if (failedStageUpdateUiValidationErrorHandler(error, form)) {
+    return;
+  }
+
+  if (failedStageUpdateUiUnexpectedErrorHandler(error)) {
+    return;
+  }
+
+  if (failedStageUpdateUiBaseErrorHandler(error)) {
+    return;
+  }
+
+  failedStageUpdateUiDefaultErrorHandler();
 }
 
-// Инициализация слушателей для обеих форм
-createPostForms.forEach((form) => formListener(form));
+function failedStageUpdateUiValidationErrorHandler(error, form) {
+  if (error instanceof ValidationError) {
+    if (Array.isArray(error.fields)) {
+      for (const field of error.fields) {
+        failedStageUpdateUiShowInputRelatedErrorInUI(field, form);
+      }
+
+      return true;
+    }
+
+    failedStageUpdateUiShowGlobalErrorInUI(error.message);
+    return true;
+  }
+
+  return false;
+}
+
+function failedStageUpdateUiShowInputRelatedErrorInUI(field, form) {
+  // ЗАДАЧА: отобразить ошибку связанную с полями ввода
+  //
+  // В form найти ноду инпута по field. Дальше в этой ноде переключить класс на ошибку
+  // И показать подсказву под нодой (вроде под нодой) (field.message)
+  const inputNode = form.querySelector("[ data-js-input]");
+  inputNode.setCustomValidity(field.message);
+
+  // debugger;
+  console.error(field);
+}
+
+function failedStageUpdateUiUnexpectedErrorHandler(error) {
+  if (error instanceof UnexpectedError) {
+    failedStageUpdateUiShowGlobalErrorInUI(error.message);
+
+    return true;
+  }
+
+  return false;
+}
+
+function failedStageUpdateUiBaseErrorHandler(error) {
+  if (error instanceof BaseError) {
+    failedStageUpdateUiShowGlobalErrorInUI(error.message);
+
+    return true;
+  }
+
+  return false;
+}
+
+function failedStageUpdateUiDefaultErrorHandler() {
+  failedStageUpdateUiShowGlobalErrorInUI(UnexpectedError.defaultMessage);
+}
+
+function failedStageUpdateUiShowGlobalErrorInUI(message) {
+  // ЗАДАЧА: отобразить глобальную (не связанную с полями ввода) ошибку
+  console.error(message);
+
+  return true;
+}
+
+[...document.querySelectorAll(".data__form")].map((f) =>
+  f.addEventListener("submit", submitListenner)
+);
 
 // Закрытие окна с положительным результатом отправки
+const resultElement = document.querySelector(".result");
+const resultCloseElement = document.querySelector(".result-close");
 
 window.addEventListener(`keydown`, (e) => {
   if (e.key === "Escape") {
@@ -205,3 +394,43 @@ window.addEventListener(`keydown`, (e) => {
 resultCloseElement.addEventListener("click", (e) => {
   resultElement.classList.add("visually-hidden");
 });
+
+// [...document.querySelectorAll(".data__form")].map((formE) => {
+//   formE.addEventListener("submit", submitListenner);
+//   const phoneNumberInput = formE.querySelector("[type=tel]");
+//   // phoneNumberInput.addEventListener("focus", (inputE) => {
+//   //   if(inputE.target.value.length === 0) {
+//   //     inputE.target.value = "+7 ("
+//   //   }
+//   // })
+
+//   phoneNumberInput.addEventListener("input", (inputE) => {
+//     const clarifiedInput = inputE.target.value.replace(/^(?:\+7|\+|7|8)/,"");
+//     const formattedPhoneNumber = `+7 ${formatPhoneNumber(clarifiedInput)}`;
+
+//     inputE.target.value =formattedPhoneNumber;
+//   })
+// });
+
+// function formatPhoneNumber(input) {
+//     const digits = input.replace(/\D/g,'').substring(0,10);
+//     const operatorCode = digits.substring(0,3);
+//     const prefix = digits.substring(3,6);
+//     const suffixA = digits.substring(6,8);
+//     const suffixB = digits.substring(8,10);
+
+//     if(digits.length > 7) {
+//       return `(${operatorCode}) ${prefix}-${suffixA}-${suffixB}`;
+//     }
+//     if(digits.length > 6) {
+//       return `(${operatorCode}) ${prefix}-${suffixA}`;
+//     }
+//     else if(digits.length > 3) {
+//       return `(${operatorCode}) ${prefix}`;
+//     }
+//     else if(digits.length > 0) {
+//       return `(${operatorCode}`;
+//     }
+
+//     return input;
+// }
